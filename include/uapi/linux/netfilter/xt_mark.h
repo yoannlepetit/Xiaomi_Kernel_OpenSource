de20bde0c8e0a487d79483255ca456ebaf32d62d
Yoann Le Petit <yoann.lepetit@yose.dev>
2024-03-20 20:30:00 +0100

    re add removed struct

--- include/uapi/linux/netfilter/xt_mark.h
+++ include/uapi/linux/netfilter/xt_mark.h
@@ -1,13 +1,17 @@
-#ifndef _XT_MARK_H_target
-#define _XT_MARK_H_target
+#ifndef _XT_MARK_H
+#define _XT_MARK_H
 
-#include <linux/netfilter/xt_mark.h>
-struct xt_mark_tginfo2 {
-	__u32 mark, mask;
+#include <linux/types.h>
+
+struct xt_mark_tginfo2
+{
+    __u32 mark, mask;
 };
 
-struct xt_mark_mtinfo1 {
-	__u32 mark, mask;
-	__u8 invert;
+struct xt_mark_mtinfo1
+{
+    __u32 mark, mask;
+    __u8 invert;
 };
-#endif /*_XT_MARK_H_target */
+
+#endif /*_XT_MARK_H*/
